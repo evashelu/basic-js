a2c5192532999c58c32a7e19d3c16f7f17ecc189
evashelu <evavolk43@gmail.com>
2025-01-06 15:22:50 +0300

    fix: fix carbon-dating & common-character functions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,23 +3,15 @@ const { NotImplementedError } = require('../extensions/index.js');
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-/**
- * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
- * @return {Number | Boolean} calculated age in years or false
- * in case of incorrect sampleActivity
- *
- * @example
- * 
- * dateSample('1') => 22387
- * dateSample('WOOT!') => false
- *
- */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(sampleActivity) || sampleActivity <= 0) {
+    return false;
+  }
+
+  const activity = parseFloat(sampleActivity);
+  const decayConstant = Math.LN2 / HALF_LIFE_PERIOD;
+  const age = Math.log(MODERN_ACTIVITY / activity) / decayConstant;
+  return age > 0 ? Math.ceil(age) : false;
 }
 
 module.exports = {
